9803a66cbe79427348dd22b6bd8401ff4bc732a4
Kris Wong <krischwong@gmail.com>
2025-07-22 23:34:20 -0400

    Used api calls for post info and comment info

--- src/Components/PostPage/PostPage.jsx
+++ src/Components/PostPage/PostPage.jsx
@@ -1,5 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+
 const PostPage = () => {
-  return <h1>Hello World</h1>;
+  const [post, setPost] = useState("");
+  const [comments, setComments] = useState("");
+  const { id } = useParams();
+
+  useEffect(() => {
+    const url = `http://localhost:3000/post/${id}`;
+    const fetchData = async () => {
+      try {
+        const data = await axios.get(`${url}`);
+        setPost(data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    const url = `http://localhost:3000/comment/${id}`;
+    const fetchData = async () => {
+      try {
+        const data = await axios.get(`${url}`);
+        setComments(data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [id]);
+
+  useEffect(() => {
+    console.log(comments);
+  }, [comments]);
+
+  return (
+    <div>
+      <div>
+        <Link to="/">Back</Link>
+        {post.title}
+        {post.content}
+        Comments
+        {comments.map((comment) => (
+          <div>{comment.content}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PostPage;
