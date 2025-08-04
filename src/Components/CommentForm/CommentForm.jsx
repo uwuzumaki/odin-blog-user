0ccd456cec9f14ddb9d03174b9b8dec69460af01
Kris Wong <krischwong@gmail.com>
2025-08-04 16:46:48 -0400

    Styling of comment form

--- src/Components/CommentForm/CommentForm.jsx
+++ src/Components/CommentForm/CommentForm.jsx
@@ -21,21 +21,38 @@ const CommentForm = ({ post_id, onUpdate }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form
+      className="mx-auto flex max-w-[70%] flex-col items-start border-r border-b border-l border-gray-300 bg-white px-12 pt-14 pb-8 md:max-w-[50%]"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <label>Name</label>
-      <input defaultValue="Anonymous" {...register("name")} />
-      <label>Comment</label>
       <input
+        className="pt-2 pb-4 focus:outline-none"
+        placeholder="Anonymous"
+        {...register("user")}
+      />
+      <label>Comment</label>
+      <textarea
+        autoComplete="off"
+        className="w-[90%] resize-none border border-gray-300"
+        rows="5"
+        cols="50"
         {...register("content", {
-          required: "Please include a comment",
+          required: "Please include a comment.",
           minLength: {
             value: 10,
             message: "Message must be at least 10 characters long.",
           },
         })}
       />
-      {errors.content && <span>{errors.content.message}</span>}
-      <input type="submit" />
+      {errors.content && (
+        <span className="text-red-500">{errors.content.message}</span>
+      )}
+      <input
+        type="submit"
+        value="Submit"
+        className="my-4 cursor-pointer rounded-xs border bg-[#f4f4f4] p-1"
+      />
     </form>
   );
 };
