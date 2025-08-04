43131fd2800b94775b98a29fe87a4a25694e7cb1
Kris Wong <krischwong@gmail.com>
2025-08-04 16:46:18 -0400

    Created loading page

--- src/Components/PostPage/PostPage.jsx
+++ src/Components/PostPage/PostPage.jsx
@@ -6,26 +6,34 @@ import CommentForm from "../CommentForm/CommentForm";
 
 const PostPage = () => {
   const [post, setPost] = useState("");
+  const [loadingPosts, setLoadingPosts] = useState(true);
   const [comments, setComments] = useState([]);
+  const [loadingComments, setLoadingComments] = useState(true);
   const { id } = useParams();
 
   const fetchPosts = async () => {
     const url = `http://localhost:3000/post/${id}`;
     try {
+      setLoadingPosts(true);
       const data = await axios.get(`${url}`);
       setPost(data.data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoadingPosts(false);
     }
   };
 
   const fetchComments = async () => {
     const url = `http://localhost:3000/comment/${id}`;
     try {
+      setLoadingComments(true);
       const data = await axios.get(url);
       setComments(data.data);
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoadingComments(false);
     }
   };
 
@@ -37,12 +45,11 @@ const PostPage = () => {
 
   const CommentRender = (commentsArray) => {
     if (commentsArray.comments.length) {
-      console.log(commentsArray);
       return (
         <>
           {comments.map((comment) => (
             <div
-              className="my-2 flex w-1/1 flex-col items-start border-b pb-2"
+              className="my-2 flex w-1/1 flex-col items-start border-b border-black/20 pb-2"
               key={comment.id}
             >
               <p className="text-[1.1rem]">{comment.name}</p>
@@ -64,32 +71,42 @@ const PostPage = () => {
       );
     }
   };
-
-  return (
-    <div className="bg-[#f4f4f4]">
-      <div className="flex">
-        <Link className="" to="/">
-          &#8249;- Back
-        </Link>
+  if (loadingComments && loadingPosts) {
+    return (
+      <>
+        {" "}
+        <div className="relative h-full min-h-0 flex-1 bg-[#f4f4f4]">
+          <span className="absolute top-[50%] animate-pulse">Loading...</span>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div className="h-full min-h-0 flex-1 bg-[#f4f4f4]">
+        <div className="flex">
+          <Link className="" to="/">
+            &#8249;- Back
+          </Link>
+        </div>
+        <div className="container mx-auto flex max-w-[70%] flex-col border border-gray-300 bg-white md:max-w-[50%]">
+          <div className="flex flex-col items-start border-b border-b-gray-300 px-12 py-14">
+            <h1 className="my-4 text-3xl font-bold">{post.title}</h1>
+            <p>{moment(post.createAt).format("MMMM Do, YYYY")}</p>
+          </div>
+          <div className="flex border-b border-b-gray-300 px-12 py-14">
+            <p className="">{post.content}</p>
+          </div>
+          <div className="flex px-12 py-14">
+            <p className="text-xl">Comments</p>
+          </div>
+          <div className="flex flex-col items-start px-12">
+            <CommentRender comments={comments} />
+          </div>
+        </div>
+        <CommentForm post_id={id} onUpdate={fetchComments} />
       </div>
-      <div className="container mx-auto flex max-w-[70%] flex-col border border-gray-300 bg-white">
-        <div className="flex flex-col items-start border-b border-b-gray-300 px-12 py-14">
-          <h1 className="my-4 text-3xl">{post.title}</h1>
-          <p>{moment(post.createAt).format("MMMM Do, YYYY")}</p>
-        </div>
-        <div className="flex border-b border-b-gray-300 px-12 py-14">
-          <p className="">{post.content}</p>
-        </div>
-        <div className="flex px-12 py-14">
-          <p className="text-xl">Comments</p>
-        </div>
-        <div className="flex flex-col items-start px-12">
-          <CommentRender comments={comments} />
-        </div>
-      </div>
-      <CommentForm post_id={id} onUpdate={fetchComments} />
-    </div>
-  );
+    );
+  }
 };
 
 export default PostPage;
