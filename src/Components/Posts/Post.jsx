9af5ca1b8e08555c2da1599036986bb680f3016d
Kris Wong <krischwong@gmail.com>
2025-08-04 11:33:49 -0400

    Styling for individual posts

--- src/Components/Posts/Post.jsx
+++ src/Components/Posts/Post.jsx
@@ -1,8 +1,19 @@
+import img from "../../assets/heroImage.jpg";
+
 const Post = (data) => {
   const post = data.post;
   return (
-    <div className="m-2 border border-solid border-slate-300 p-4">
-      <div>{post.title}</div>
+    <div className="mx-4 my-2 flex h-64 w-full flex-col rounded-2xl border border-solid border-slate-300 p-4 shadow duration-100 ease-linear hover:shadow-xl">
+      <div className="overflow-hidden text-ellipsis whitespace-nowrap">
+        {post.title}
+      </div>
+      <div className="relative flex-1">
+        <img
+          src={img}
+          // Centres the image inside the wrapper
+          className="absolute top-1/2 left-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 object-cover md:max-w-[256px]"
+        />
+      </div>
     </div>
   );
 };
